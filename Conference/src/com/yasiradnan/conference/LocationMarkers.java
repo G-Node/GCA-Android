25e44b910fbb5a4f36319aa3ee73d20af73030be
Yasir Adnan <adnan.ayon@gmail.com>
2013-06-17 21:40:58 +0600

    Created a class for Location Markers

---
+++ Conference/src/com/yasiradnan/conference/LocationMarkers.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.yasiradnan.conference;
+
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * @author Adnan
+ *
+ */
+public class LocationMarkers extends FragmentActivity {
+
+}
