32065d4a04727c6978fdb37c9eaf43ba80aac196
Yasir Adnan <adnan.ayon@gmail.com>
2013-07-11 11:21:11 +0600

    created abstract content layout

--- Conference/gen/com/yasiradnan/conference/R.java
+++ Conference/gen/com/yasiradnan/conference/R.java
@@ -220,40 +220,43 @@ containing a value of this type.
         public static final int white_right=0x7f020035;
     }
     public static final class id {
-        public static final int action_settings=0x7f060013;
+        public static final int abTopic=0x7f060005;
+        public static final int abType=0x7f060006;
+        public static final int action_settings=0x7f060015;
         public static final int hybrid=0x7f060004;
-        public static final int ibLeft=0x7f06000f;
-        public static final int ibRight=0x7f060010;
-        public static final int icon=0x7f060011;
-        public static final int itemArrow=0x7f06000d;
-        public static final int list=0x7f060006;
-        public static final int map=0x7f06000e;
+        public static final int ibLeft=0x7f060011;
+        public static final int ibRight=0x7f060012;
+        public static final int icon=0x7f060013;
+        public static final int itemArrow=0x7f06000f;
+        public static final int list=0x7f060008;
+        public static final int map=0x7f060010;
         public static final int none=0x7f060000;
         public static final int normal=0x7f060001;
-        public static final int pager=0x7f060008;
+        public static final int pager=0x7f06000a;
         public static final int satellite=0x7f060002;
         public static final int terrain=0x7f060003;
-        public static final int text=0x7f060007;
-        public static final int textView1=0x7f060009;
-        public static final int textView2=0x7f06000a;
-        public static final int title=0x7f060012;
-        public static final int tvDay=0x7f060005;
-        public static final int tvTime=0x7f06000b;
-        public static final int tvTitle=0x7f06000c;
+        public static final int text=0x7f060009;
+        public static final int textView1=0x7f06000b;
+        public static final int textView2=0x7f06000c;
+        public static final int title=0x7f060014;
+        public static final int tvDay=0x7f060007;
+        public static final int tvTime=0x7f06000d;
+        public static final int tvTitle=0x7f06000e;
     }
     public static final class layout {
-        public static final int abstract_general=0x7f030000;
-        public static final int activity_general=0x7f030001;
-        public static final int activity_screen_slide=0x7f030002;
-        public static final int content_extended=0x7f030003;
-        public static final int list_break=0x7f030004;
-        public static final int list_event_with_content=0x7f030005;
-        public static final int list_event_without_content=0x7f030006;
-        public static final int list_separator=0x7f030007;
-        public static final int location_marker=0x7f030008;
-        public static final int main=0x7f030009;
-        public static final int schedule=0x7f03000a;
-        public static final int tab_indicator=0x7f03000b;
+        public static final int abstract_content=0x7f030000;
+        public static final int abstract_general=0x7f030001;
+        public static final int activity_general=0x7f030002;
+        public static final int activity_screen_slide=0x7f030003;
+        public static final int content_extended=0x7f030004;
+        public static final int list_break=0x7f030005;
+        public static final int list_event_with_content=0x7f030006;
+        public static final int list_event_without_content=0x7f030007;
+        public static final int list_separator=0x7f030008;
+        public static final int location_marker=0x7f030009;
+        public static final int main=0x7f03000a;
+        public static final int schedule=0x7f03000b;
+        public static final int tab_indicator=0x7f03000c;
     }
     public static final class menu {
         public static final int general=0x7f0a0000;
