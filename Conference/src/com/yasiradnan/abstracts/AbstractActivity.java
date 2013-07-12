f2a72dda5157e60b2c224de86e020f99ee5b6f52
Yasir Adnan <adnan.ayon@gmail.com>
2013-07-12 10:46:20 +0600

    Search Filter update

--- Conference/src/com/yasiradnan/abstracts/AbstractActivity.java
+++ Conference/src/com/yasiradnan/abstracts/AbstractActivity.java
@@ -1,5 +1,4 @@
 
-
 package com.yasiradnan.abstracts;
 
 import java.io.BufferedReader;
@@ -15,22 +14,18 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
-
-
-
-
-
-
-
 import com.yasiradnan.conference.R;
 
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
@@ -39,8 +34,8 @@ import android.widget.TextView;
  * @author Adnan
  */
 public class AbstractActivity extends ListActivity {
-    
-    static final List<HashMap<String, String>> list = new ArrayList<HashMap<String, String>>();
+
+    static final List<HashMap<String, String>> listitems = new ArrayList<HashMap<String, String>>();
 
     static final String[] from = new String[] {
             "topic", "type", "title", "type", "abstract"
@@ -54,72 +49,110 @@ public class AbstractActivity extends ListActivity {
 
     public static final String KEY_TYPE = "type";
 
+    SimpleAdapter simpleAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
+        
         setContentView(R.layout.abstract_general);
+        
         datainList();
+
+        /*
+         * Serach Filter
+         */
+        
+        EditText searchOption = (EditText)findViewById(R.id.abSearch);
+        
+        searchOption.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // TODO Auto-generated method stub
+                AbstractActivity.this.simpleAdapter.getFilter().filter(s);
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // TODO Auto-generated method stub
+
+            }
+        });
+
     }
 
     private void datainList() {
         try {
-           
-            BufferedReader jsonReader = new BufferedReader(new InputStreamReader(this
-                    .getResources().openRawResource(R.raw.abstracts)));
-            StringBuilder jsonBuilder = new StringBuilder();
-            for (String line = null; (line = jsonReader.readLine()) != null;) {
-                jsonBuilder.append(line).append("\n");
+            if (listitems.isEmpty()) {
+                BufferedReader jsonReader = new BufferedReader(new InputStreamReader(this
+                        .getResources().openRawResource(R.raw.abstracts)));
+                StringBuilder jsonBuilder = new StringBuilder();
+                for (String line = null; (line = jsonReader.readLine()) != null;) {
+                    jsonBuilder.append(line).append("\n");
+                }
+
+                JSONTokener tokener = new JSONTokener(jsonBuilder.toString());
+                JSONArray jsonArray = new JSONArray(tokener);
+
+                for (int index = 0; index < jsonArray.length(); index++) {
+
+                    JSONObject jsonObject = jsonArray.getJSONObject(index);
+
+                    String topic = jsonObject.getString("topic");
+
+                    Log.e("topic", topic);
+
+                    String type = jsonObject.getString("type");
+
+                    String title = jsonObject.getString("title");
+
+                    String absData = jsonObject.getString("abstract");
+
+                    HashMap<String, String> addingData = new HashMap<String, String>();
+
+                    addingData.put(KEY_TOPIC, topic);
+                    addingData.put(KEY_TYPE, type);
+                    addingData.put(KEY_TITLE, title);
+                    addingData.put(KEY_ABSTRACT, absData);
+
+                    listitems.add(addingData);
+
+                }
             }
-
-           
-            JSONTokener tokener = new JSONTokener(jsonBuilder.toString());
-            JSONArray jsonArray = new JSONArray(tokener);
-
-            for (int index = 0; index < jsonArray.length(); index++) {
-                
-                
-                JSONObject jsonObject = jsonArray.getJSONObject(index);
-
-                String topic = jsonObject.getString("topic");
-                
-                Log.e("topic", topic);
-                
-                String type = jsonObject.getString("type");
-
-                String title = jsonObject.getString("title");
-
-                String absData = jsonObject.getString("abstract");
-                
-                HashMap<String, String> temp = new HashMap<String, String>();
-
-                temp.put(KEY_TOPIC, topic);
-                temp.put(KEY_TYPE, type);
-                temp.put(KEY_TITLE, title);
-                temp.put(KEY_ABSTRACT, absData);
-
-                list.add(temp);
-
-            }
-            SimpleAdapter sm = new SimpleAdapter(this, list, R.layout.abstract_content, from,
+            simpleAdapter = new SimpleAdapter(this, listitems, R.layout.abstract_content, from,
                     new int[] {
                             R.id.abTopic, R.id.abType, R.id.abTitle, R.id.SubTitle
                     });
-            setListAdapter(sm);
+            setListAdapter(simpleAdapter);
+
             ListView listView = getListView();
-            
+
             listView.setOnItemClickListener(new OnItemClickListener() {
                 @Override
-                public void onItemClick(AdapterView<?> parent, View v,
-                        int position, long id) {
+                public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
                     // TODO Auto-generated method stub
-                    //Get Abstract Data
-                    String abstracts_content = list.get(position).get(KEY_ABSTRACT).toString();
-                    //Create a new Intent
-                    Intent in = new Intent(getApplicationContext(),AbstractContent.class);
-                    //Getting Abstract for another activity
+                    // Get Abstract Data
+                    
+                    String abstracts_content = listitems.get(position).get(KEY_ABSTRACT).toString();
+                    
+                    // Create a new Intent
+                    
+                    Intent in = new Intent(getApplicationContext(), AbstractContent.class);
+                    
+                    // Getting Abstract for another activity
+                    
                     in.putExtra("abstracts", abstracts_content);
-                    //StratActivity
+                    
+                    // StratActivity
+                    
                     startActivity(in);
                 }
             });
