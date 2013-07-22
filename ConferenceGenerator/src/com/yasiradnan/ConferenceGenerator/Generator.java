fd31af3cd08812474c1e3d89500edce50f9af1f4
Yasir Adnan <adnan.ayon@gmail.com>
2013-07-23 00:01:47 +0600

    Created Generator Class

---
+++ ConferenceGenerator/src/com/yasiradnan/ConferenceGenerator/Generator.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.yasiradnan.ConferenceGenerator;
+
+/**
+ * @author Adnan
+ *
+ */
+public class Generator {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
