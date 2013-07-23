033a81726f180c6ce16d53db2d8bc76767159428
Yasir Adnan <adnan.ayon@gmail.com>
2013-07-23 23:56:39 +0600

    Created AbstractCursorAdapter

---
+++ Conference/src/com/yasiradnan/abstracts/AbstractCursorAdapter.java
@@ -0,0 +1,29 @@
+package com.yasiradnan.abstracts;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.support.v4.widget.CursorAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AbstractCursorAdapter extends CursorAdapter {
+
+    @SuppressWarnings("deprecation")
+    public AbstractCursorAdapter(Context context, Cursor c) {
+        super(context, c);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void bindView(View arg0, Context arg1, Cursor arg2) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public View newView(Context arg0, Cursor arg1, ViewGroup arg2) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
