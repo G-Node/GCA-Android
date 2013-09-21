c7c46246ffaec50c52c4cdee0fe441ffbc815986
Yasir Adnan <adnan.ayon@gmail.com>
2013-09-21 11:17:33 +0600

    Fixes for Commenting, Blank Spaces

--- Conference/src/org/g_node/location/LocationMarkers.java
+++ Conference/src/org/g_node/location/LocationMarkers.java
@@ -1,6 +1,7 @@
 /**
  * 
  */
+
 package org.g_node.location;
 
 import java.io.BufferedReader;
@@ -9,6 +10,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
+import org.g_node.gcaa.R;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -30,139 +32,188 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.yasiradnan.conference.R;
-import com.yasiradnan.conference.R.drawable;
-import com.yasiradnan.conference.R.id;
-import com.yasiradnan.conference.R.layout;
-import com.yasiradnan.conference.R.raw;
+
 /**
  * @author Adnan
- *
  */
+
 public class LocationMarkers extends FragmentActivity {
 
-	private static GoogleMap supportMap;
-	private ArrayList<LatLng> allCoordinates = new ArrayList<LatLng>();
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.location_marker);
-		try {
-		     MapsInitializer.initialize(this);
-		 } catch (GooglePlayServicesNotAvailableException e) {
-		     e.printStackTrace();
-		 }
-		locationMarkers();
-		
-	}
-	
-	public void locationMarkers()
-	{
-		/*Implement Location Markers*/
-		BufferedReader jsonReader = new BufferedReader(new InputStreamReader(this.getResources().openRawResource(R.raw.map)));
-		StringBuilder jsonBuilder = new StringBuilder();
-		try {
-			for (String line = null; (line = jsonReader.readLine()) != null;) {
-				jsonBuilder.append(line).append("\n");
-			}
-			
-			JSONTokener tokener = new JSONTokener(jsonBuilder.toString());
-			JSONArray jsonArray = new JSONArray(tokener);
-			
-			
-			for (int index = 0; index < jsonArray.length(); index++) 
-			{
-				
-				JSONObject jsonObject = jsonArray.getJSONObject(index);
-				/*get Latitude*/
-				double getLat = jsonObject.getJSONObject("point").getDouble("lat");
-				/*get Longitude*/
-				double getLng = jsonObject.getJSONObject("point").getDouble("long");
-				/*get Location Type*/
-				int gettype = jsonObject.getInt("type");
-				/**/
-				int getZoomto = jsonObject.getInt("zoomto");
-				/*Venue name*/
-				String name   = jsonObject.getString("name");
-				/**/
-				LatLng myLoc = new LatLng(getLat, getLng);
-				if(getZoomto == 1)
-				{	
-					/*only Food and Venue*/
-					allCoordinates.add(myLoc);
-					
-				}
-				FragmentManager fmanager = getSupportFragmentManager();
-		        Fragment fragment = fmanager.findFragmentById(R.id.map);
-				SupportMapFragment supportmapfragment = (SupportMapFragment)fragment;
-				supportMap = supportmapfragment.getMap();
-			        if(supportMap!=null){
-			        	/*implementing custom markers for different location's*/
-			        	switch (gettype) {
-						case 0:
-							/*Conference Venue Marker*/
-							supportMap.addMarker(new MarkerOptions().position(myLoc)
-				        	          .title(name).icon(BitmapDescriptorFactory.fromResource(R.drawable.conference)));
-							break;
-						case 1:
-							/*University Marker*/
-							supportMap.addMarker(new MarkerOptions().position(myLoc)
-				        	          .title(name).icon(BitmapDescriptorFactory.fromResource(R.drawable.university)));
-							break;
-						case 2:
-							/*Hotel -1 Marker*/
-							supportMap.addMarker(new MarkerOptions().position(myLoc)
-				        	          .title(name).icon(BitmapDescriptorFactory.fromResource(R.drawable.hotel_1)));
-							break;
-						case 3:
-							/*Hotel -2 Marker*/
-							supportMap.addMarker(new MarkerOptions().position(myLoc)
-				        	          .title(name).icon(BitmapDescriptorFactory.fromResource(R.drawable.hotel_2)));
-							break;
-						case 4:
-							/*Transport Marker*/
-							supportMap.addMarker(new MarkerOptions().position(myLoc)
-				        	          .title(name).icon(BitmapDescriptorFactory.fromResource(R.drawable.transport)));
-							break;
-						case 5:
-							/*Food Marker*/
-							supportMap.addMarker(new MarkerOptions().position(myLoc)
-				        	          .title(name).icon(BitmapDescriptorFactory.fromResource(R.drawable.food)));
-							break;
+    private static GoogleMap supportMap;
 
-						default:
-							break;
-						}
-			        }
-			      
-			}
-			
-			 /*Automatic zoom level*/
-			 LatLngBounds.Builder builder = new LatLngBounds.Builder();
-	        	for(LatLng m : allCoordinates) {
-	        	    builder = builder.include(m);
-	        	    }
-	        	LatLngBounds bounds = builder.build();
-	        	CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, 
-	                    this.getResources().getDisplayMetrics().widthPixels, 
-	                    this.getResources().getDisplayMetrics().heightPixels, 
-	                    50);
-	        			/*Move Camera*/
-	        			supportMap.moveCamera(cu);
-	        			/*Set My Current Location Enable*/
-			     		supportMap.setMyLocationEnabled(true);
-			        	supportMap.getUiSettings().setMyLocationButtonEnabled(true);
-			        	/*Set Comapass Enable*/
-			        	supportMap.getUiSettings().setCompassEnabled(true);
-			        	/*Set ZoomControl Enable*/
-			        	supportMap.getUiSettings().setZoomControlsEnabled(false);
-		} catch (FileNotFoundException e) {
-			Log.e("jsonFile", "file not found");
-		} catch (IOException e) {
-			Log.e("jsonFile", "ioerror");
-		} catch (JSONException e) {
-			Log.e("jsonFile", Log.getStackTraceString(e));
-		}
-		
-	}
+    private ArrayList<LatLng> allCoordinates = new ArrayList<LatLng>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.location_marker);
+        try {
+            MapsInitializer.initialize(this);
+        } catch (GooglePlayServicesNotAvailableException e) {
+            e.printStackTrace();
+        }
+        locationMarkers();
+
+    }
+
+    public void locationMarkers() {
+        /*
+         * Implement Location Markers
+         */
+        BufferedReader jsonReader = new BufferedReader(new InputStreamReader(this.getResources()
+                .openRawResource(R.raw.map)));
+        StringBuilder jsonBuilder = new StringBuilder();
+        try {
+            for (String line = null; (line = jsonReader.readLine()) != null;) {
+                jsonBuilder.append(line).append("\n");
+            }
+
+            JSONTokener tokener = new JSONTokener(jsonBuilder.toString());
+            JSONArray jsonArray = new JSONArray(tokener);
+
+            for (int index = 0; index < jsonArray.length(); index++) {
+
+                JSONObject jsonObject = jsonArray.getJSONObject(index);
+                /*
+                 * getting Latitude
+                 */
+                double getLat = jsonObject.getJSONObject("point").getDouble("lat");
+                /*
+                 * getting Longitude
+                 */
+                double getLng = jsonObject.getJSONObject("point").getDouble("long");
+                /*
+                 * getting Location Type
+                 */
+                int gettype = jsonObject.getInt("type");
+                /*
+                 * getting zoomto value
+                 */
+                int getZoomto = jsonObject.getInt("zoomto");
+                /*
+                 * Venue name
+                 */
+                String name = jsonObject.getString("name");
+                LatLng myLoc = new LatLng(getLat, getLng);
+                if (getZoomto == 1) {
+                    /*
+                     * Adding only food and venue coordinates for automatic zoom
+                     * level
+                     */
+                    allCoordinates.add(myLoc);
+                }
+                FragmentManager fmanager = getSupportFragmentManager();
+                Fragment fragment = fmanager.findFragmentById(R.id.map);
+                SupportMapFragment supportmapfragment = (SupportMapFragment)fragment;
+                supportMap = supportmapfragment.getMap();
+                if (supportMap != null) {
+                    /*
+                     * implementing different colors markers for different
+                     * location's
+                     */
+                    switch (gettype) {
+                        case 0:
+                            /*
+                             * Conference Venue Marker
+                             */
+                            supportMap.addMarker(new MarkerOptions()
+                                    .position(myLoc)
+                                    .title(name)
+                                    .icon(BitmapDescriptorFactory
+                                            .fromResource(R.drawable.conference)));
+                            break;
+                        case 1:
+                            /*
+                             * University Marker
+                             */
+                            supportMap.addMarker(new MarkerOptions()
+                                    .position(myLoc)
+                                    .title(name)
+                                    .icon(BitmapDescriptorFactory
+                                            .fromResource(R.drawable.university)));
+                            break;
+                        case 2:
+                            /*
+                             * Hotel -1 Marker
+                             */
+                            supportMap
+                                    .addMarker(new MarkerOptions()
+                                            .position(myLoc)
+                                            .title(name)
+                                            .icon(BitmapDescriptorFactory
+                                                    .fromResource(R.drawable.hotel_1)));
+                            break;
+                        case 3:
+                            /*
+                             * Hotel -2 Marker
+                             */
+                            supportMap
+                                    .addMarker(new MarkerOptions()
+                                            .position(myLoc)
+                                            .title(name)
+                                            .icon(BitmapDescriptorFactory
+                                                    .fromResource(R.drawable.hotel_2)));
+                            break;
+                        case 4:
+                            /*
+                             * Transport Marker
+                             */
+                            supportMap.addMarker(new MarkerOptions()
+                                    .position(myLoc)
+                                    .title(name)
+                                    .icon(BitmapDescriptorFactory
+                                            .fromResource(R.drawable.transport)));
+                            break;
+                        case 5:
+                            /*
+                             * Food Marker
+                             */
+                            supportMap.addMarker(new MarkerOptions().position(myLoc).title(name)
+                                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.food)));
+                            break;
+
+                        default:
+                            break;
+                    }
+                }
+
+            }
+
+            /*
+             * Automatic zoom level
+             */
+            LatLngBounds.Builder builder = new LatLngBounds.Builder();
+            for (LatLng m : allCoordinates) {
+                builder = builder.include(m);
+            }
+            LatLngBounds bounds = builder.build();
+            CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, this.getResources()
+                    .getDisplayMetrics().widthPixels,
+                    this.getResources().getDisplayMetrics().heightPixels, 50);
+            /*
+             * Move Camera
+             */
+            supportMap.moveCamera(cu);
+            /*
+             * Set My Current Location Enable
+             */
+            supportMap.setMyLocationEnabled(true);
+            supportMap.getUiSettings().setMyLocationButtonEnabled(true);
+            /*
+             * Set Compass Enable
+             */
+            supportMap.getUiSettings().setCompassEnabled(true);
+            /*
+             * Set Manual ZoomControl Enable
+             */
+            supportMap.getUiSettings().setZoomControlsEnabled(false);
+        } catch (FileNotFoundException e) {
+            Log.e("jsonFile", "file not found");
+        } catch (IOException e) {
+            Log.e("jsonFile", "ioerror");
+        } catch (JSONException e) {
+            Log.e("jsonFile", Log.getStackTraceString(e));
+        }
+
+    }
 }
