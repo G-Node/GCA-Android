2defd92fd3a9707ddc4f754400778c955dce6eba
Yasir Adnan <adnan.ayon@gmail.com>
2013-09-21 13:18:09 +0600

    Fixes for org.g_node.gcaa

--- Conference/src/org/g_node/gcaa/GeneralActivity.java
+++ Conference/src/org/g_node/gcaa/GeneralActivity.java
@@ -1,75 +1,69 @@
+
 package org.g_node.gcaa;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-
-
-
-
 import com.petebevin.markdown.MarkdownProcessor;
-import com.yasiradnan.conference.R;
-
 import android.app.Activity;
 import android.content.Context;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.text.Html;
-import android.view.Window;
 import android.widget.TextView;
 
 public class GeneralActivity extends Activity {
-	TextView text;
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_general);
-		/*initial UI*/
-		initialUI();
-		
-		/*Read file*/
-		String raw=readRawTextFile(this, R.raw.information);
-		
-		/**/
-		MarkdownProcessor markDownPro = new MarkdownProcessor();
-		
-		/**/
-		String getMarkDown = markDownPro.markdown(raw);
-		
-		/**/
-		CharSequence cs = Html.fromHtml(getMarkDown);
-		
-		/**/
-		text.setText(cs);
-		
-	}
-	
-	private void initialUI(){
-		text = (TextView)findViewById(R.id.text);
-	}
-	
-	/**/
-	
-	public static String readRawTextFile(Context ctx, int resId) {
-	    InputStream inputStream=ctx.getResources().openRawResource(resId);
-	    InputStreamReader inputreader=new InputStreamReader(inputStream);
-	    BufferedReader buffreader=new BufferedReader(inputreader);
-	    String line;
-	    StringBuilder text=new StringBuilder();
+    TextView text;
 
-	    try {
-	      while ((line=buffreader.readLine())!=null) {
-	        text.append(line);
-	        text.append('\n');
-	      }
-	    }
-	    catch (IOException e) {
-	      return null;
-	    }
-	    return text.toString();
-	  }
-	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_general);
+        /*
+         * initializing UI
+         */
+        initialUI();
+        /*
+         * Read file
+         */
+        String raw = readRawTextFile(this, R.raw.information);
+        MarkdownProcessor markDownPro = new MarkdownProcessor();
+        /*
+         * Get Content as MarkDown
+         */
+        String getMarkDown = markDownPro.markdown(raw);
+        /*
+         * Getting content as CharSequence
+         */
+        CharSequence cs = Html.fromHtml(getMarkDown);
+        /*
+         * Set Content in TextView
+         */
+        text.setText(cs);
+
+    }
+
+    private void initialUI() {
+        text = (TextView)findViewById(R.id.text);
+    }
+
+    public static String readRawTextFile(Context ctx, int resId) {
+        InputStream inputStream = ctx.getResources().openRawResource(resId);
+        InputStreamReader inputreader = new InputStreamReader(inputStream);
+        BufferedReader buffreader = new BufferedReader(inputreader);
+        String line;
+        StringBuilder text = new StringBuilder();
+
+        try {
+            while ((line = buffreader.readLine()) != null) {
+                text.append(line);
+                text.append('\n');
+            }
+        } catch (IOException e) {
+            return null;
+        }
+        return text.toString();
+    }
+
 }
--- Conference/src/org/g_node/gcaa/Tabs.java
+++ Conference/src/org/g_node/gcaa/Tabs.java
@@ -1,15 +1,12 @@
 /**
  * 
  */
+
 package org.g_node.gcaa;
 
-import org.g_node. schedule.ScheduleMainActivity;
-import org.g_node. schedule.ScheduleSlideFragment;
+import org.g_node.schedule.ScheduleMainActivity;
 import org.g_node.abstracts.AbstractActivity;
 import org.g_node.location.LocationMarkers;
-
-import com.yasiradnan.conference.R;
-
 import android.app.TabActivity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -22,47 +19,47 @@ import android.widget.TextView;
 
 /**
  * @author Adnan
- *
  */
 @SuppressWarnings("deprecation")
 public class Tabs extends TabActivity {
-/** Called when the activity is first created. */
-	
-	public void onCreate(Bundle savedInstanceState) {
-		
-	    super.onCreate(savedInstanceState);
-	 
-	    
-	    this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+    /** Called when the activity is first created. */
 
-		setContentView(R.layout.main);
-		
-		setTabs() ;
-	}
-	private void setTabs()
-	{
-		addTab("Info", R.drawable.tab_home, GeneralActivity.class);
-		addTab("Program", R.drawable.tab_search, ScheduleMainActivity.class);
-		
-		addTab("Abstracts", R.drawable.tab_home,AbstractActivity.class);
-		addTab("Map", R.drawable.tab_map,LocationMarkers.class);
-	}
-	
-	private void addTab(String labelId, int drawableId, Class<?> c)
-	{
-		TabHost tabHost = getTabHost();
-		Intent intent = new Intent(this, c);
-		TabHost.TabSpec spec = tabHost.newTabSpec("tab" + labelId);	
-		
-		View tabIndicator = LayoutInflater.from(this).inflate(R.layout.tab_indicator, getTabWidget(), false);
-		TextView title = (TextView) tabIndicator.findViewById(R.id.title);
-		title.setText(labelId);
-		ImageView icon = (ImageView) tabIndicator.findViewById(R.id.icon);
-		icon.setImageResource(drawableId);
-		
-		spec.setIndicator(tabIndicator);
-		spec.setContent(intent);
-		tabHost.addTab(spec);
-	}
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        /*
+         * Disable ActionBar
+         */
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.main);
+        setTabs();
+    }
+
+    private void setTabs() {
+        /*
+         * Add Activities with Tabs
+         */
+        addTab("Info", R.drawable.tab_home, GeneralActivity.class);
+        addTab("Program", R.drawable.tab_search, ScheduleMainActivity.class);
+        addTab("Abstracts", R.drawable.tab_home, AbstractActivity.class);
+        addTab("Map", R.drawable.tab_map, LocationMarkers.class);
+    }
+
+    private void addTab(String labelId, int drawableId, Class<?> c) {
+        TabHost tabHost = getTabHost();
+        Intent intent = new Intent(this, c);
+        TabHost.TabSpec spec = tabHost.newTabSpec("tab" + labelId);
+
+        View tabIndicator = LayoutInflater.from(this).inflate(R.layout.tab_indicator,
+                getTabWidget(), false);
+        TextView title = (TextView)tabIndicator.findViewById(R.id.title);
+        title.setText(labelId);
+        ImageView icon = (ImageView)tabIndicator.findViewById(R.id.icon);
+        icon.setImageResource(drawableId);
+
+        spec.setIndicator(tabIndicator);
+        spec.setContent(intent);
+        tabHost.addTab(spec);
+    }
 
 }
