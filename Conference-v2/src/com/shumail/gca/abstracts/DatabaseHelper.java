ad75c50d3f31a81d2adbce748e0310ac49b171b5
Shumail Mohy-ud-Din <shumailmohyuddin@gmail.com>
2014-06-16 01:20:28 +0500

    DBHelper class for database functionality
    DBHelper class for database connectivity, and functions

---
+++ Conference-v2/src/com/shumail/gca/abstracts/DatabaseHelper.java
@@ -0,0 +1,33 @@
+/**
+* Date: Jun 16, 2014
+* Author: Shumail Mohy-ud-Din
+*/
+
+package com.shumail.gca.abstracts;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper{
+
+	public DatabaseHelper(Context context, String name, CursorFactory factory,
+			int version) {
+		super(context, name, factory, version);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
