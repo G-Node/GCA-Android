c0dcf7619be91a9decb6b070f37a711b3fcd8230
Shumail Mohy-ud-Din <shumailmohyuddin@gmail.com>
2014-06-26 23:59:59 +0500

    Basic Splash Screen added with 1 sec interval
    Basic Splash Screen added with 1 sec interval

---
+++ Conference-v2/src/com/shumail/gca/SplashScreen.java
@@ -0,0 +1,42 @@
+package com.shumail.gca;
+
+import com.shumail.newsroom.R;
+import com.shumail.newsroom.R.layout;
+import com.shumail.newsroom.R.menu;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.Window;
+
+public class SplashScreen extends Activity {
+	
+	// Splash screen timer
+    private static int SPLASH_TIME_OUT = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //removing the action bar of splash screen
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+        getActionBar().hide();
+        setContentView(R.layout.activity_splash_screen);
+ 
+        new Handler().postDelayed(new Runnable() {
+        	//Handler for for showing splash screen for a little time
+        	
+            @Override
+            public void run() {
+                //Start activity Tabs
+                Intent mainActivity = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(mainActivity);
+                
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+}
