34b4f6c140772f56c614e0978c843c3ea8077ede
Shumail Mohy-ud-Din <shumailmohyuddin@gmail.com>
2014-07-08 00:20:27 +0500

    Implemented Insert New Note with DialogFragment
    Implemented Insert New Note with DialogFragment

--- Conference-v2/src/com/g_node/gca/abstracts/AbstractNotesFragment.java
+++ Conference-v2/src/com/g_node/gca/abstracts/AbstractNotesFragment.java
@@ -3,15 +3,19 @@ package com.g_node.gca.abstracts;
 import com.googlecode.androidannotations.annotations.res.StringArrayRes;
 import com.shumail.newsroom.R;
 
+import android.app.DialogFragment;
+import android.app.FragmentManager;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.webkit.WebView.FindListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
@@ -52,6 +56,17 @@ public class AbstractNotesFragment extends Fragment {
         
         //ArrayAdapter adapter = new ArrayAdapter<String>(this,  R.layout.noteslist_item_layout, StringArray);
         notesList.setAdapter(adapter);
+        
+        Button addNoteBtn = (Button) getView().findViewById(R.id.addNotebtn);
+        addNoteBtn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				AddNoteDialogFragment addNoteFragment = AddNoteDialogFragment.newInstance(value);
+				addNoteFragment.show(getFragmentManager(), "fragment_add_note");
+			}
+		});
         
 	}
 	
---
+++ Conference-v2/src/com/g_node/gca/abstracts/AddNoteDialogFragment.java
@@ -0,0 +1,88 @@
+package com.g_node.gca.abstracts;
+
+import com.shumail.newsroom.R;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.support.v4.app.DialogFragment;
+import android.text.Editable;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class AddNoteDialogFragment extends DialogFragment {
+
+	
+	public AddNoteDialogFragment(){
+	    // Empty constructor required for DialogFragment
+	}
+	
+	public static AddNoteDialogFragment newInstance(String uuid) {
+		AddNoteDialogFragment frag = new AddNoteDialogFragment();
+        Bundle args = new Bundle();
+        args.putString("uuid", uuid);
+        frag.setArguments(args);
+        return frag;
+    }
+	
+//	 @Override
+//	    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//	            Bundle savedInstanceState) {
+//		 
+//				 
+		 
+//		 
+//	        View view = inflater.inflate(R.layout.add_note_dialog_fragment, container);
+//	        EditText mEditText = (EditText) view.findViewById(R.id.txt_your_name);
+//	        String uuid = getArguments().getString("uuid", "uuid not valid");
+//	        getDialog().setTitle("Add New Note:");
+//	        // Show soft keyboard automatically
+//	        mEditText.requestFocus();
+//	        
+//	        return view;
+//	    }
+	 
+	 @Override
+     public Dialog onCreateDialog(Bundle savedInstanceState) {
+		 AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getActivity());
+		 
+		 final View view = getActivity().getLayoutInflater().inflate(R.layout.add_note_dialog_fragment, null);
+		 final String uuid = getArguments().getString("uuid", "uuid not valid");
+         alertDialogBuilder.setView(view);
+         alertDialogBuilder.setTitle("Add New Note: ");
+         alertDialogBuilder.setPositiveButton("ADD", new DialogInterface.OnClickListener() {
+             @Override
+             public void onClick(DialogInterface dialog, int which) {
+            	 
+            	 EditText noteTextEditText = (EditText) view.findViewById(R.id.noteText);
+            	 EditText noteTitleEditText = (EditText) view.findViewById(R.id.noteTitle2);
+
+            	 String absUUID = uuid;
+            	 Editable noteText = noteTextEditText.getText();
+            	 String y = noteText.toString();
+            	 String noteTitle = noteTitleEditText.getText().toString();
+            	 DatabaseHelper.addInABSTRACT_NOTES(absUUID, y, noteTitle);
+                 Toast.makeText(getActivity(), "ADDED ..", Toast.LENGTH_SHORT).show();
+                 dialog.dismiss();
+             }
+         });
+         alertDialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+             @Override
+             public void onClick(DialogInterface dialog, int which) {
+                 Toast.makeText(getActivity(), "Cancelled" , Toast.LENGTH_SHORT).show();
+                 dialog.cancel();
+             }
+         });
+
+         return alertDialogBuilder.create();
+
+	 }
+
+}
