fac506bc20fa1d3c6eea74f0b6ae439ec15e50a7
Shumail Mohy-ud-Din <shumailmohyuddin@gmail.com>
2014-07-24 02:24:52 +0500

    Extracted Abstract UUID from Event and open relevant Abstract ( #13 )
    Abstract UUID is extracted from Event Abstract JSON URL and then checked
    in db if that UUID exists. If it exists, then that Abstract is displayed
    otherwise user is informed that no Abstract is present for this UUID.
    (#13)

--- Conference-v2/src/com/g_node/gca/schedule/ScheduleItemExtended.java
+++ Conference-v2/src/com/g_node/gca/schedule/ScheduleItemExtended.java
@@ -1,13 +1,20 @@
 package com.g_node.gca.schedule;
 
+import com.g_node.gca.abstracts.AbstractContent;
+import com.g_node.gca.abstracts.DatabaseHelper;
 import com.shumail.newsroom.R;
 import com.shumail.newsroom.R.layout;
 import com.shumail.newsroom.R.menu;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class ScheduleItemExtended extends Activity {
@@ -15,10 +22,18 @@ public class ScheduleItemExtended extends Activity {
 	private String SCHEDULE_ITEMTYPE_EVENT = "event";
 	private String SCHEDULE_ITEMTYPE_TRACK = "track";
 	private String SCHEDULE_ITEMTYPE_SESSION = "session";
+	
+	DatabaseHelper dbHelper = new DatabaseHelper(this);;
+	
+	String event_abstract_uuid;
+	
+	String LOG_TAG = "GCA-Sch-Extend";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		dbHelper.open();
 		
 		Bundle bundle = getIntent().getExtras();
 		String eType = bundle.getString("type");
@@ -32,7 +47,38 @@ public class ScheduleItemExtended extends Activity {
 			
 			TextView eventTitleView = (TextView) findViewById(R.id.scheduleDetailTitle);
 			eventTitleView.setText(eventToDisplay.getTitle());
-		
+			
+			event_abstract_uuid = eventToDisplay.getEventAbstract();
+			event_abstract_uuid = event_abstract_uuid.substring(event_abstract_uuid.lastIndexOf("/")+1, event_abstract_uuid.length());
+			Log.i(LOG_TAG, "Abstract ID of event: " + event_abstract_uuid);
+			
+	        String query = "SELECT UUID , TOPIC, TITLE, ABSRACT_TEXT, STATE, SORTID, REASONFORTALK, MTIME, TYPE, DOI, COI, ACKNOWLEDGEMENTS FROM ABSTRACT_DETAILS where UUID = '" + event_abstract_uuid + "';";
+			
+			Cursor abstractForEventCursor = DatabaseHelper.database.rawQuery(query, null);
+			
+			Button btnOpenAbstract = (Button) findViewById(R.id.btn_launch_Abstract_from_event);
+			
+			if(abstractForEventCursor.getCount() < 1) {
+				btnOpenAbstract.setEnabled(false);
+				btnOpenAbstract.setText("No Abstract Found");
+				//btnOpenAbstract.setVisibility(View.GONE);
+			} else {
+				
+				btnOpenAbstract.setOnClickListener(new OnClickListener() {
+					
+					@Override
+					public void onClick(View arg0) {
+						// TODO Auto-generated method stub
+						
+						Intent intent = new Intent(ScheduleItemExtended.this, AbstractContent.class);
+						intent.putExtra("value", event_abstract_uuid);
+						startActivity(intent);
+					}
+				});
+				
+			}
+			
+			
 		} else if (eType.equals(SCHEDULE_ITEMTYPE_TRACK)) {
 		
 			setContentView(R.layout.activity_schedule_item_extended);
@@ -60,5 +106,14 @@ public class ScheduleItemExtended extends Activity {
 		getMenuInflater().inflate(R.menu.schedule_item_extended, menu);
 		return true;
 	}
+
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		dbHelper.close();
+		super.onDestroy();
+	}
+	
+
 
 }
