b0bc12ced33be680e7cd2e88d245088744433873
cgars <christian@stuebeweg50.de>
2016-08-04 10:56:48 +0200

    Imp: Use same layout for events in session and Track

--- Conference/src/com/g_node/gca/schedule/scheduleAdapter.java
+++ Conference/src/com/g_node/gca/schedule/scheduleAdapter.java
@@ -70,32 +70,12 @@ public class scheduleAdapter extends BaseAdapter {
 		ScheduleItemRecord currSchedItem = scheduleItemsGeneralList.get(arg0);
 		if(currSchedItem.getSchedule_item_type().equals(SCHEDULE_ITEMTYPE_EVENT)) {
 			Log.i("GCA-B-Schedule", "GetView called for EVENT");
+			
 			vi = inflater.inflate(R.layout.schedule_list_events_general, null);
+			EventScheduleItem tempEvent = eventsRecordsList.get(
+					currSchedItem.getIndex());
 			
-			if(vi==null ){
-				Log.i("error", "null");
-			}
-			TextView x = (TextView) vi.findViewById(R.id.event_start_time);
-			int indexOfEvent = currSchedItem.getIndex();
-			
-			EventScheduleItem tempEvent = eventsRecordsList.get(indexOfEvent);
-			
-			x.setText(tempEvent.getStart());
-			
-			TextView xa = (TextView) vi.findViewById(R.id.event_title);
-			xa.setText(tempEvent.getTitle());
-			
-			((TextView)vi.findViewById(R.id.event_end_time)).setText(tempEvent.getEnd());
-			((TextView)vi.findViewById(R.id.event_location)).setText(tempEvent.getLocation());
-			
-			if(!tempEvent.getAuthors().equals("")) 
-				((TextView)vi.findViewById(R.id.event_location)).append(Html.fromHtml("&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;<i>" + tempEvent.getAuthors() + "</i>" ));
-			
-			if (tempEvent.getType().equals("food")){
-				vi.findViewById(R.id.list_item_middle_container).setBackgroundColor(
-						ctx.getResources().getColor(R.color.color_food));
-			}			
-			vi.setOnClickListener(new ScheduleEventOnClickListener(tempEvent));
+			inserEventData(vi, tempEvent);			
 			return vi;
 		
 		} else if (currSchedItem.getSchedule_item_type().equals(SCHEDULE_ITEMTYPE_TRACK)) {	
@@ -104,19 +84,10 @@ public class scheduleAdapter extends BaseAdapter {
 			
 			//display track here now
 			vi = inflater.inflate(R.layout.schedule_list_track, null);
-			
-			if(vi==null ){
-				Log.i("error", "null");
-			}
-			
-			TextView x = (TextView) vi.findViewById(R.id.trackTitle);
-			
-			int indexOfTrack = currSchedItem.getIndex();
-			
-			TrackScheduleItem tempTrack = tracksRecordsList.get(indexOfTrack);
-			
+			TextView x = (TextView) vi.findViewById(R.id.trackTitle);			
+			int indexOfTrack = currSchedItem.getIndex();			
+			TrackScheduleItem tempTrack = tracksRecordsList.get(indexOfTrack);			
 			x.setText(tempTrack.getTitle());			
-			
 			if (tempTrack.getChair().length()>0){
 				TextView xa = (TextView) vi.findViewById(R.id.trackSubtitle);
 				xa.setText(ctx.getResources().getString(R.string.track_chair_label)
@@ -126,27 +97,8 @@ public class scheduleAdapter extends BaseAdapter {
 			TableLayout table = (TableLayout)vi.findViewById(R.id.trackEventsTable);
 			
 			for(int i=0; i<tempTrackEvents.length; i++) {				
-				TableRow tempRow = (TableRow) LayoutInflater.from(ctx).
-						inflate(R.layout.track_events_table_row, null);
-				((TextView)tempRow.findViewById(R.id.track_event_start)).
-				setText(tempTrackEvents[i].getStart());
-				((TextView)tempRow.findViewById(R.id.track_event_title)).
-				setText(tempTrackEvents[i].getTitle());
-				((TextView)tempRow.findViewById(R.id.track_event_location)).
-				setText(tempTrackEvents[i].getLocation());
-				((TextView)tempRow.findViewById(R.id.track_Event_end)).
-				setText(tempTrackEvents[i].getEnd());
-				
-				if(!tempTrackEvents[i].getAuthors().equals("")) 
-					((TextView)tempRow.findViewById(R.id.track_event_location)).
-					append(Html.fromHtml("&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;<i>" + 
-					tempTrackEvents[i].getAuthors() + "</i>"));
-				
-				if (tempTrackEvents[i].getType().equals("food")){
-					tempRow.findViewById(R.id.list_item_middle_container).setBackgroundColor(
-							ctx.getResources().getColor(R.color.color_food));
-				}
-				tempRow.setOnClickListener(new ScheduleEventOnClickListener(tempTrackEvents[i]));
+				View tempRow = inflater.inflate(R.layout.schedule_list_events_general, null);
+				inserEventData(tempRow, tempTrackEvents[i]);
 				table.addView(tempRow);
 			}
 			table.requestLayout();
@@ -156,83 +108,48 @@ public class scheduleAdapter extends BaseAdapter {
 		} else {
 			Log.i("GCA-B-Schedule", "GetView called for SESSION");		
 			vi = inflater.inflate(R.layout.schedule_list_session, null);
+			SessionScheduleItem currSession = sessionsRecordList.get(
+					currSchedItem.getIndex());			
+			((TextView) vi.findViewById(R.id.sessionTitle)).setText(
+					currSession.getTitle());
+			((TextView) vi.findViewById(R.id.sessionSubtitle)).
+			setText(currSession.getSubtitle() );
 			
-			if(vi==null ){
-				Log.i("error", "null");
-			}
-			TextView sessionTitle = (TextView) vi.findViewById(R.id.sessionTitle);
+			TrackScheduleItem[] currSessionTracks = currSession.getTracksInSession();						
+			TableLayout table = (TableLayout)vi.findViewById(R.id.sessionTracksTable);			
 			
-			int indexOfSession = currSchedItem.getIndex();
-			
-			SessionScheduleItem tempSession = sessionsRecordList.get(indexOfSession);
-			
-			sessionTitle.setText(tempSession.getTitle());
-			
-			TextView sessionSubtitle = (TextView) vi.findViewById(R.id.sessionSubtitle);
-			sessionSubtitle.setText(tempSession.getSubtitle() );
-			
-			TrackScheduleItem[] tempSessionTracks = tempSession.getTracksInSession();
-						
-			TableLayout table = (TableLayout)vi.findViewById(R.id.sessionTracksTable);
-			
-			Log.i("GCA-A-Schedule", "Session Tracks Count: " + tempSessionTracks.length);
-			
+			Log.i("GCA-A-Schedule", "Session Tracks Count: " + currSessionTracks.length);
 			//add tracks to table
-			for(int i=0; i<tempSessionTracks.length; i++) {				
-				Log.i("GCA-A-Schedule", "in outer Loop no: " + i);				
-				TableRow tempRow = (TableRow) inflater.inflate(R.layout.session_track_table_row, null);
-				((TextView)tempRow.findViewById(R.id.session_track_name)).
-				setText(tempSessionTracks[i].getTitle());
-				if (tempSessionTracks[i].getChair().length() > 0){
-						((TextView)tempRow.findViewById(R.id.session_track_chair)).
-						setText(ctx.getResources().getString(R.string.track_chair_label) + tempSessionTracks[i].getChair());
+			for(int i=0; i<currSessionTracks.length; i++) {				
+				TableRow sessionRow = (TableRow) inflater.inflate(
+						R.layout.session_track_table_row, null);
+				((TextView)sessionRow.findViewById(R.id.session_track_name)).
+				setText(currSessionTracks[i].getTitle());
+				if (currSessionTracks[i].getChair().length() > 0){
+						((TextView)sessionRow.findViewById(R.id.session_track_chair)).
+						setText(ctx.getResources().getString(
+								R.string.track_chair_label) + 
+								currSessionTracks[i].getChair());
 				}
+				
 				//here add events of respective track in another table
-				EventScheduleItem[] eventsInCurrentTrack = tempSessionTracks[i].getEventsInTrack();
-				
-				TableLayout trackEventstable = (TableLayout)tempRow.findViewById(R.id.session_track_events_table);
-				
-				Log.i("GCA-A-Schedule", "Track Events Count: " + eventsInCurrentTrack.length);
-				
+				EventScheduleItem[] eventsInCurrentTrack = currSessionTracks[i].
+						getEventsInTrack();
+				TableLayout trackEventsTable = (TableLayout)sessionRow.
+						findViewById(R.id.session_track_events_table);				
+				Log.i("GCA-A-Schedule", "Track Events Count: " + 
+						eventsInCurrentTrack.length);				
 				for(int j=0; j<eventsInCurrentTrack.length; j++) {
-					Log.i("GCA-A-Schedule", "in loop: " + j);
 					//adding each event into a this table
-					TableRow tempEventRowForTrackEventsTable = (TableRow) inflater.inflate(R.layout.session_track_event_table_row, null);
-					if(tempEventRowForTrackEventsTable == null){
-						Log.i("GCA-Schedule", "NULL SCENE");
-					}
-					((TextView)tempEventRowForTrackEventsTable.findViewById(
-							R.id.session_track_event_start)).
-							setText(eventsInCurrentTrack[j].getStart());
-					((TextView)tempEventRowForTrackEventsTable.findViewById(
-							R.id.session_track_Event_end)).setText(
-									eventsInCurrentTrack[j].getEnd());
-					((TextView)tempEventRowForTrackEventsTable.findViewById(
-							R.id.session_track_event_title)).setText(
-									eventsInCurrentTrack[j].getTitle());
-					((TextView)tempEventRowForTrackEventsTable.findViewById(
-							R.id.session_track_event_location)).setText(
-									eventsInCurrentTrack[j].getLocation());
-					
-					if(!eventsInCurrentTrack[j].getAuthors().equals("")) 
-						((TextView)tempEventRowForTrackEventsTable.findViewById(
-								R.id.session_track_event_location)).append(
-										Html.fromHtml("&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;<i>" 
-								+ eventsInCurrentTrack[j].getAuthors() + "</i>"));
-					
-					if (eventsInCurrentTrack[j].getType().equals("food")){
-						tempRow.findViewById(R.id.list_item_middle_container).setBackgroundColor(
-								ctx.getResources().getColor(R.color.color_food));
-					}
-					tempEventRowForTrackEventsTable.setOnClickListener(
-							new ScheduleEventOnClickListener(eventsInCurrentTrack[j]));
-					//Adding the event row to Tracks
-					trackEventstable.addView(tempEventRowForTrackEventsTable);
-					trackEventstable.requestLayout();
+					View trackRow = inflater.inflate(
+							R.layout.schedule_list_events_general, null);
+					inserEventData(trackRow, eventsInCurrentTrack[j]);
+					trackEventsTable.addView(trackRow);
+					trackEventsTable.requestLayout();
 				}
 								
 				//adding the final track row
-				table.addView(tempRow);
+				table.addView(sessionRow);
 			}
 			table.requestLayout();
 			
@@ -242,6 +159,34 @@ public class scheduleAdapter extends BaseAdapter {
 		} //end if/else
 		
 	} //end getView
+
+	private void inserEventData(View currView, EventScheduleItem currEvent) {
+		((TextView)currView.findViewById(R.id.event_start_time)).
+		setText(currEvent.getStart());
+		((TextView)currView.findViewById(R.id.event_end_time)).
+		setText(currEvent.getEnd());
+		((TextView)currView.findViewById(R.id.event_location)).
+		setText(currEvent.getLocation());
+		((TextView)currView.findViewById(R.id.event_title)).
+		setText(currEvent.getTitle());
+		if(!currEvent.getAuthors().equals("")) 
+			((TextView)currView.findViewById(R.id.event_location)).
+			append(Html.fromHtml("&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;<i>" + 
+			currEvent.getAuthors() + "</i>" ));
+		
+		if (currEvent.getType().equals("food")){
+			currView.findViewById(R.id.list_item_middle_container).setBackgroundColor(
+					ctx.getResources().getColor(R.color.color_food));
+		}
+		
+		if (currEvent.getType().equals("keynote")){
+			TextView eventTypeTV = (TextView) currView.findViewById(R.id.event_type);
+			eventTypeTV.setVisibility(View.VISIBLE);
+			eventTypeTV.setText(currView.getResources().getString(
+					R.string.schedule_event_keynote));				
+		}
+		currView.setOnClickListener(new ScheduleEventOnClickListener(currEvent));
+	}
 
 	private class ScheduleEventOnClickListener implements OnClickListener
 	{
