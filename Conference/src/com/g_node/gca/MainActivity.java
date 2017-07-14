2aa39434a83d1c36ceb6ba11b50799e0856d2e79
cgars <christian@stuebeweg50.de>
2017-07-14 11:10:29 +0200

    Remove buttons from  mainUI and use menu instead

--- Conference/src/com/g_node/gca/MainActivity.java
+++ Conference/src/com/g_node/gca/MainActivity.java
@@ -24,7 +24,7 @@ import com.g_node.gca.abstracts.FavoriteAbstracts;
 import com.g_node.gca.map.MapActivity;
 import com.g_node.gca.newsroom.NewsRoomActivity;
 import com.g_node.gca.schedule.ScheduleMainActivity;
-import com.g_node.gcaa.R;
+import com.g_node.ni17.R;
 
 public class MainActivity extends Activity {
 	
@@ -34,73 +34,7 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		
 		getActionBar().setTitle(R.string.app_name);
-		//getActionBar().setIcon(getResources().getDrawable(R.drawable.icon_brain));
-		
-		Button news = (Button)findViewById(R.id.btn_news);
-		news.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				Intent newsIntent = new Intent(MainActivity.this, NewsRoomActivity.class);
-				startActivity(newsIntent);
-			}
-		});
-		
-		Button absBtn = (Button) findViewById(R.id.btn_abstracts);
-		absBtn.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				Intent abstractIntent = new Intent(MainActivity.this, Abstracts.class);
-				startActivity(abstractIntent);
-				
-			}
-		});
-		
-		Button mapBtn = (Button) findViewById(R.id.btn_maps);
-		mapBtn.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				Intent mapIntent = new Intent(MainActivity.this, MapActivity.class);
-				startActivity(mapIntent);
-				
-			}
-		});
-		
-		Button scheduleBtn = (Button) findViewById(R.id.btn_schedule);
-		scheduleBtn.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				Intent scheduleIntent = new Intent(MainActivity.this, ScheduleMainActivity.class);
-				startActivity(scheduleIntent);
-				
-			}
-		});
-
-		
-		Button favBtn = (Button) findViewById(R.id.btn_favorites);
-		favBtn.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				Intent favIntent = new Intent(MainActivity.this, FavoriteAbstracts.class);
-				startActivity(favIntent);
-				
-			}
-		});
-		
-		Button infBtn = (Button) findViewById(R.id.btn_info);
-		infBtn.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				Intent infIntent = new Intent(MainActivity.this, GeneralActivity.class);
-				startActivity(infIntent);
-				
-			}
-		});
+		//getActionBar().setIcon(getResources().getDrawable(R.drawable.icon_brain));	
 		
 	}
 
@@ -118,19 +52,30 @@ public class MainActivity extends Activity {
 		case R.id.genInfo:
             Intent genInfo = new Intent(this, GeneralActivity.class);
             this.startActivity(genInfo);
+            return true;		
+		case R.id.abstracts_menue:
+			Intent abstractIntent = new Intent(MainActivity.this, Abstracts.class);
+			startActivity(abstractIntent);
             return true;
-        
+		case R.id.schedule_menue:
+			Intent scheduleIntent = new Intent(MainActivity.this, ScheduleMainActivity.class);
+			startActivity(scheduleIntent);
+            return true;
+		case R.id.favorites_menue:
+			Intent favIntent = new Intent(MainActivity.this, FavoriteAbstracts.class);
+			startActivity(favIntent);
+            return true;
 		case R.id.abtApp:
 		{
 			Builder aboutDialog = new AlertDialog.Builder(MainActivity.this);
 			aboutDialog.setTitle("About the App:")
-			.setMessage(Html.fromHtml("The <b>G-Node Conference Application</b> for Android serves as an electronic conference guide for participants with included proceedings. <br><br> &#169; <b>German Neuroinformatics Node</b><br><br>Created By: <b>Shumail Mohy-ud-Din</b><br>(as part of GSoC 2014)"))
+			.setMessage(Html.fromHtml("The <b>G-Node Conference Application</b> for Android serves as an electronic conference guide for participants with included proceedings. <br><br> &#169; <b>German Neuroinformatics Node</b><br><br>Created By: <b>Shumail Mohy-ud-Din</b> and <b>Christian Garbers</b><br>(as part of GSoC 2014)"))
 			.setNeutralButton(android.R.string.ok,
 					new DialogInterface.OnClickListener() {
 				public void onClick(DialogInterface dialog, int id) {
 					dialog.cancel();
 				}
-			}).setIcon(getResources().getDrawable(R.drawable.launcher_brain_icon))
+			}).setIcon(getResources().getDrawable(R.drawable.launcher))
 			 .show();
 		}	
 	        return true;
